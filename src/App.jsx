7814e4fbc3754188f529da8f792e2de05e5d7693
senthil <connectmsk@gmail.com>
2024-11-27 17:53:36 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import MemoryGame from "./components/memory-game";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return <MemoryGame />;
+}
+
+export default App;
